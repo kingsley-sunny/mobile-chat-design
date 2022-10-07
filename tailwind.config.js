5fe1516de122c8e1dac203744ede49a243562286
kingsley-sunny <ezeobisunny51@gmail.com>
2022-10-07 13:30:07 +0100

    added a gitignore file and changed the index.html

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,18 @@
 module.exports = {
-  mode: "jit",
-  purge: [
-    "./public/**/*.html"
-  ],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {
-      colors: {
-        "light-magenta": "hsl(293, 100%, 63%)",
-        "light-violet": "hsl(264, 100%, 61%)",
-        'yello': "#baa333"
-      }
+    mode: "jit",
+    purge: ["./*.html"],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {
+            colors: {
+                "light-magenta": "hsl(293, 100%, 63%)",
+                "light-violet": "hsl(264, 100%, 61%)",
+                yello: "#baa333",
+            },
+        },
     },
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
-}
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+};
